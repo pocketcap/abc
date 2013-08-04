8f92956874023466d4d2e78119e7d395ebea794a
jlburkhead <jlburkhead@ucdavis.edu>
2013-08-04 13:06:00 +0000

    cleaning directories for github

--- R/auc.R
+++
@@ -1,9 +0,0 @@
-auc <- function(outcome, proba)
-    {
-        N = length(proba)
-        N_pos = sum(outcome)
-        df = data.frame(out = outcome, prob = proba)
-        df = df[order(-df$prob),]
-        df$above = (1:N) - cumsum(df$out)
-        return( 1- sum( df$above * df$out ) / (N_pos * (N-N_pos) ) )
-    }
--- R/dbCon.R
+++
@@ -1,5 +0,0 @@
-suppressPackageStartupMessages(library(RMySQL))
-
-cred <- readLines("~/mysql_cred.txt")
-con <- dbConnect(MySQL(), user = cred[1], dbname = "sbdb", host = cred[3], password = cred[2])
-
